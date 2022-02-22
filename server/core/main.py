60b1e1764a40651e71095a247a794277b0dca3fb
alvinsetya <comand.alvin@gmail.com>
2022-02-22 18:35:52 +0800

    CREATED REPORTER, SERVER UNTESTED

--- server/core/main.py
+++ server/core/main.py
@@ -1,6 +1,8 @@
+from traceback import format_exc as f_exc
 import os
 import socket
-
+import select
+from server.tools import report
 
 class Server(socket.socket):
     def __init__(self, ip_address=socket.gethostbyname(socket.gethostname()), port=5000):
@@ -12,17 +14,51 @@ class Server(socket.socket):
         self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         # flags and vars
-        self.started = False
-        self.sockets_ = [self]      # list of all socket including server socket itself
-        self.sockets_info = {}      # list of all client socket and their basic information
+        self.is_started = False
+        self.sockets_r = [self]      # list of all socket to be read including server socket itself
+        self.sockets_w = [self]      # list of all socket to be wrote including server socket itself
+        self.sockets_err = [self]    # list of all socket to be write error including server socket itself
+        self.sockets_info = {}       # list of all client socket and their basic information
+        self.reporter = report.Log()
 
+    def __del__(self):
+        self.sockets_.clear()
+        self.sockets_info.clear()
+        self.close()
 
     def bind_server(self, max_backlog=5):
         try:
             self.bind((self.ip_address, self.port))
             self.listen(max_backlog)
-            
 
         except:
-            # report exception to log
-            pass
+            self.reporter.report(f_exc(), self.reporter.error)
+
+    def halt(self):
+        self.sockets_.pop(0)
+
+        for client_socket in self.sockets_:
+            self.send_data(client_socket, b"0x1")
+            client_socket.close()
+
+        
+
+    def start(self):
+        if not self.is_started:
+            return "Server is not running yet!"
+
+        while self.is_started:
+            try:
+                for incoming_socket in select.select(self.sockets_r, self.sockets_w, self.sockets_err):
+                    if not incoming_socket:
+                        continue
+
+                    print(incoming_socket)
+
+            except KeyboardInterrupt:
+                # ask to halt the server
+                pass
+
+            except:
+                self.reporter.report(f_exc(), self.reporter.error)
+            
--- server/tools/report.py
+++ server/tools/report.py
@@ -1,9 +1,80 @@
 # modules for displaying server activities and write to a log file 
 
 
+from colorama import init
+from termcolor import colored
 import logging
-import colorama
 import datetime
+import os
 
-logging.basicConfig(filename=datetime.datetime.strftime("%d-%m-%Y-%H:%M"), filemode='w')
+init()
 
+class Log:
+    def __init__(self, log_level=logging.DEBUG, name="SYSTEM"):
+        self.log_level = log_level
+        self.log_ = logging.getLogger(name)
+        self.log_.setLevel(self.log_level)
+        self.log_.addHandler(self.get_file_handler())
+   
+    @property
+    def debug(self):
+        return colored("DEBUG", "white"), self.log_.debug
+
+    @property
+    def info(self):
+        return colored("INFO", "green"), self.log_.info
+
+    @property
+    def warning(self):
+        return colored("WARNING", "yellow"),self.log_.warning
+
+    @property
+    def error(self):
+        return colored("ERROR", "red"), self.log_.error
+
+    @property
+    def critical(self):
+        return colored("CRITICAL", "grey"), self.log_.critical
+
+
+    def get_file_handler(self, fname=datetime.datetime.now().strftime('%d-%m-%Y-%H:%M.log')):
+        f_handler = logging.FileHandler(fname, mode="w+")
+        f_format = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s => %(message)s")
+        f_handler.setFormatter(f_format)
+        f_handler.setLevel(self.log_level)
+
+        return f_handler
+
+
+    def init_check(self):
+        # check logs directory if doesnt exists then it will create automatically
+        # else it will use existing one
+
+        if not os.path.isdir("logs"):
+            os.mkdir("logs")
+
+
+    def report(self, message, exception_type, display_mode=print, *display_mode_args, **display_mode_kwargs):
+        """
+        Available execption type:
+
+        1. debug => no mark
+        2. info => green mark
+        3. warning => yellow mark
+        4. error => red mark
+        5. critical => lightred mark
+        """
+
+        exception_type[1](message)
+        message = datetime.datetime.now().strftime("[%a, %b-%Y %H:%M]") + f"[{exception_type[0]}]> " + message
+        display_mode(message, *display_mode_args, **display_mode_kwargs)
+        
+
+
+def __test():
+    d = Log()
+    d.report("Hello Worlds", d.info)
+
+
+if __name__ == "__main__":
+    __test()
